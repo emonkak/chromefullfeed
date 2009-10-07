c0e466a31b8f46b4937f4e38a7cb88c1ec04194a
Constellation <utatane.tea@gmail.com>
2009-10-07 20:50:09 +0900

    * fix createHTML and $X (thanks id:os0x and id:nanto_vi)

--- src/chromefullfeed.js
+++ src/chromefullfeed.js
@@ -311,18 +311,52 @@
   function $A(arr){
     return Array.prototype.slice.call(arr);
   }
-  // simple version of $X
+  // XPath 式中の接頭辞のない名前テストに接頭辞 prefix を追加する
+  // e.g. '//body[@class = "foo"]/p' -> '//prefix:body[@class = "foo"]/prefix:p'
+  // http://nanto.asablo.jp/blog/2008/12/11/4003371
+  function addDefaultPrefix(xpath, prefix) {
+    var tokenPattern = /([A-Za-z_\u00c0-\ufffd][\w\-.\u00b7-\ufffd]*|\*)\s*(::?|\()?|(".*?"|'.*?'|\d+(?:\.\d*)?|\.(?:\.|\d+)?|[\)\]])|(\/\/?|!=|[<>]=?|[\(\[|,=+-])|([@$])/g;
+    var TERM = 1, OPERATOR = 2, MODIFIER = 3;
+    var tokenType = OPERATOR;
+    prefix += ':';
+    function replacer(token, identifier, suffix, term, operator, modifier) {
+      if (suffix) {
+        tokenType =
+          (suffix == ':' || (suffix == '::' && (identifier == 'attribute' || identifier == 'namespace')))
+          ? MODIFIER : OPERATOR;
+      } else if (identifier) {
+        if (tokenType == OPERATOR && identifier != '*')
+          token = prefix + token;
+        tokenType = (tokenType == TERM) ? OPERATOR : TERM;
+      } else {
+        tokenType = term ? TERM : operator ? OPERATOR : MODIFIER;
+      }
+      return token;
+    }
+    return xpath.replace(tokenPattern, replacer);
+  }
+
+  // $X on XHTML
   // $X(exp);
   // $X(exp, context);
-  // @source http://gist.github.com/3242.txt
+  // @target Freifox3, Chrome3, Safari4, Opera10
+  // @source http://gist.github.com/184276.txt
   function $X (exp, context) {
     context || (context = document);
-    var expr = (context.ownerDocument || context).createExpression(exp, function (prefix) {
-      return document.createNSResolver(context.documentElement || context).lookupNamespaceURI(prefix) ||
-        context.namespaceURI || document.documentElement.namespaceURI || "";
-    });
+    var _document  = context.ownerDocument || document,
+    documentElement = _document.documentElement;
+    var isXHTML = documentElement.tagName !== 'HTML' && _document.createElement('p').tagName === 'p';
+    var defaultPrefix = null;
+    if (isXHTML) {
+      defaultPrefix = '__default__';
+      exp = addDefaultPrefix(exp, defaultPrefix);
+    }
+    function resolver (prefix) {
+      return context.lookupNamespaceURI(prefix === defaultPrefix ? null : prefix) ||
+           documentElement.namespaceURI || '';
+    }
 
-    var result = expr.evaluate(context, XPathResult.ANY_TYPE, null);
+    var result = _document.evaluate(exp, context, resolver, XPathResult.ANY_TYPE, null);
       switch (result.resultType) {
         case XPathResult.STRING_TYPE : return result.stringValue;
         case XPathResult.NUMBER_TYPE : return result.numberValue;
@@ -405,8 +439,11 @@
   }
   function parse(str, link) {
     try {
+      // thanks id:os0x
       var htmldoc = document.implementation.createHTMLDocument('fullfeed');
-      var df = $CF(str);
+      var range = document.createRange();
+      range.selectNodeContents(htmldoc.body);
+      var df = range.createContextualFragment(str);
       htmldoc.body.appendChild(df);
       remove_risks(htmldoc);
       var resolver = path_resolver(link);
